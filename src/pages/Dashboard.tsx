52e9e9c4de4ec25880acb537bcb63c03ca9e8a57
Hector Paulo <adrian.paulo.vaz@gmail.com>
2024-10-30 08:21:55 -0600

    correciones e inclusión de foto importante

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,6 +3,11 @@ import { Card, CardContent, CardHeader } from "@mui/material";
 export const Dashboard = () => (
     <Card>
         <CardHeader title="Bienvenido a la papuadministración" />
-        <CardContent>Saludos a los camaradas...</CardContent>
+        <CardContent>
+            <h3>¡Saludos a los papucamaradas!</h3>
+        </CardContent>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <img style={{ width: "400px" }} src="../../public/Assets/important-photo.png" alt="important" />
+        </div>
     </Card>
 );
